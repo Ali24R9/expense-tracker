5225195544ca32f673bc90156a6b9660d9cc7a09
Ali Abou-Zahr & Elikem Adadevoh <student@epicodus.com>
2014-02-26 17:26:15 -0800

    added purchases, categories not working yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,178 +1,54 @@
-var Contact = {
-  all: [],
-  initialize: function(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName; 
-    this.addresses = []; 
-    this.number = [];  
-  },
-  create: function(firstName, lastName) {
-    var contact = Object.create(Contact);
-    contact.initialize(firstName, lastName);
-    Contact.all.push(contact);
-    return contact;
-  },
-  createAddress: function(street, city, state){
-    var address = Object.create(Address.create(street, city, state));
 
-    this.addresses.push(address);
-    return address;
-  }, 
+var Category = {
+  initialize: function(category) {
+    this.category = category;
+    this.purchase = [];
 
-  createPhone: function(number){
-    var number = Object.create(Phone.create(number));
-    
-    this.number.push(number);
-    return number;
-  }, 
-
-  fullName: function() {
-    return this.firstName + " " + this.lastName;
   },
+  create: function(category) {
+    var category = Object.create(Category);
+    category.initialize(category);
+    return category;
+  },
+  createPurchase: function(description, amount) {
+    var purchases = Object.create(Purchase.create(description, amount));
+    this.purchase.push(purchases);
+    return purchases;
+  }
 };
 
-var Address = {
-  all: [],
-
-  initialize: function(street, city, state) {
-    this.street = street;
-    this.city = city;
-    this.state = state;
+var Purchase = {
+  initialize: function(description, amount) {
+    this.description = description;
+    this.amount = amount;  
   },
-
-  create: function(street, city, state) {
-    var address = Object.create(Address);
-    address.initialize(street, city, state);
-    Address.all.push(address);
-    return address; 
-  }, 
-
-  validateAddress: function() {
-    if((this.street !== "") && (this.city !== "") && (this.state !== "")) {
-      return true;
-    }
-    else {
-      return false;
-    }
-
+  create: function(description, amount) {
+    var purchase = Object.create(Purchase);
+    purchase.initialize(description, amount);
+    return purchase;
   },
-
-  fullAddress: function() {
-    return this.street + ', ' + this.city + ', ' + this.state;
-  }
-
-};
-
-var Phone = {
-  initialize: function(number) {
-    this.number = number;
-  },
-
-  create: function(number) {
-    var phone = Object.create(Phone);
-    phone.initialize(number);
-    return phone;
-  },
-
-  validateNumber: function() {
-    if((this.number !== "") && (!(isNaN(this.number)))) {
-      return true;
-    } else {
-      return false;
-    }
-  },
-
-  showNumber: function() {
-    return this.number;
-  }
 };
 
 $(document).ready(function() {
-  $('#add-address').click(function() {
-    $('#new-addresses').append(
-          '<div class="new-address">' + 
-             '<div class="form-group">' + 
-               '<label for="new-street">Street</label>' + 
-               '<input type="text" class="form-control new-street">' + 
-             '</div>' + 
-             '<div class="form-group">' + 
-               '<label for="new-city">City</label>' + 
-               '<input type="text" class="form-control new-city">' + 
-             '</div>' + 
-             '<div class="form-group">' + 
-               '<label for="new-state">State</label>' + 
-               '<input type="text" class="form-control new-state">' + 
-             '</div>' + 
-             '<div class="form-group">' + 
-               '<label for="new-phone">Phone Number</label>' + 
-               '<input type="text" class="form-control new-phone">' + 
-             '</div>' + 
-           '</div>'); 
-        });
-  
+  // var currentCategory = "";
+  // $('form#category').submit(function(event) {
+  //   event.preventDefault();
+  //   var inputtedCategory = $('#category').val();
+  //   var newCategory = Category.create(inputtedCategory)
+  // });
 
-  $('form#new-contact').submit(function(event){
+  $('form#purchases').submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirst = $('#new-first-name').val();
-    var inputtedLast = $('#new-last-name').val();
-
-    var newContact = Contact.create(inputtedFirst ,inputtedLast);
-
-    newContact.phoneNumbers = [];
-    $('.new-address').each(function() {
-      var inputtedStreet = $(this).find('.new-street').val();
-      var inputtedCity = $(this).find('.new-city').val();
-      var inputtedState = $(this).find('.new-state').val();
-      var inputtedPhone = $(this).find('.new-phone').val();
-
-      newContact.createAddress(inputtedStreet, inputtedCity, inputtedState);
-      newContact.createPhone(inputtedPhone);                    
-      // var newAddress = Object.create(Address);
-      // newAddress.street = inputtedStreet;
-      // newAddress.city = inputtedCity;
-      // newAddress.state = inputtedState;
-
-      //var newPhoneNumber = Object.create(Phone);
-      //newPhoneNumber.number = inputtedPhone;
-
-      // newContact.addresses.push(newAddress);
-      // newContact.phoneNumbers.push(newPhoneNumber);
-    });
-
-
-    var allAddressesValid = newContact.addresses.every(function(address) {
-      return address.validateAddress();
-    });
-
-    var allNumbersValid = newContact.number.every(function(num) {
-      return num.validateNumber();
-    });
-
-
-    if (allAddressesValid && allNumbersValid) {
-      $('ul#contacts').append('<li><span class="contact">' + newContact.fullName() + "</span></li>");
-
-      $('.contact').last().click(function() {
-      $('#show-contact').show();
-
-      $('h2#contactInfo').text(newContact.fullName());
-      $('.first-name').text(newContact.firstName);
-      $('.last-name').text(newContact.lastName);
-      $('ul#numbers').text("");
-      $('ul#addresses').text("");
-
-      newContact.number.forEach(function(nums) {
-        console.log(nums);
-        $('ul#numbers').append("<li>" + nums.number + "</li>");
-      });
-      newContact.addresses.forEach(function(address) {
-        $('ul#addresses').append("<li>" + address.fullAddress() + "</li>");
-        });
-      });
-    } else {
-      alert('fail!')
-    }
+    var inputtedDescription = $('#description').val();
+    var inputtedAmount = $('#amount').val();
+    //var newPurchase = newCategory.createPurchase(inputtedDescription, inputtedAmount);
+    var newPurchase = Purchase.create(inputtedDescription, inputtedAmount);
+    $("#descriptionAmount").prepend("<tr><td>" + newPurchase.description + "</td><td>" + newPurchase.amount + "</td></tr>");
+    this.reset();
+  });
+  $('form#categoryList').submit(function(event) {
+    event.preventDefault();
 
     this.reset();
   });
--- spec/specs.js
+++ spec/specs.js
@@ -1,155 +1,67 @@
-beforeEach(function() {
-  Contact.all = [];
-});
-
-describe('Contact', function() {
+describe('Category', function() {
   describe("create", function() {
-    it("creates a new instance of a Contact", function() {
-      var testContact = Contact.create();
-      Contact.isPrototypeOf(testContact).should.equal(true);
+    it("creates a new instanceCategory of a Category", function() {
+      var testCategory = Category.create();
+      Category.isPrototypeOf(testCategory).should.equal(true);
     });
 
-    it("initializes the contact", function() {
-      var testContact = Contact.create("Mary", "Jane");
-      testContact.addresses.should.eql([]);
-    });
-
-    it("adds the contact to the .all property", function() {
-      var testContact = Contact.create();
-      Contact.all.should.eql([testContact]);
+    it("initializes the category", function() {
+      var testCategory = Category.create("food");
+      testCategory.purchase.should.eql([]);
     });
   });
-   describe("createAddress", function() {
-    it("creates an address object", function() {
-      var testContact = Contact.create();
-      var testAddress = testContact.createAddress();
-      Address.isPrototypeOf(testAddress).should.equal(true);
+   describe("createPurchase", function() {
+    it("creates an purchase object", function() {
+      var testCategory = Category.create();
+      var testPurchase = testCategory.createPurchase();
+      Purchase.isPrototypeOf(testPurchase).should.equal(true);
     });
 
-    it("adds the address to the addresses property of the contact", function() {
-      var testContact = Contact.create();
-      var testAddress = testContact.createAddress();
-      testContact.addresses.should.eql([testAddress]);
+    it("adds the purchase to the purchase property of the contact", function() {
+      var testCategory = Category.create("food");
+      var testPurchase = testCategory.createPurchase("burger","10");
+      testCategory.purchase.should.eql([testPurchase]);
     });
   });
 
-  describe('fullName', function() {
-    it('should put a first and last name together', function() {
-      var testContact = Object.create(Contact);
-      testContact.firstName = "Dolly";
-      testContact.lastName = "Pardon";
-      testContact.fullName().should.equal('Dolly Pardon');
-    });
-  }); 
+
 
   describe("initialize", function() {
     it("sets the first and last name", function() {
-      var testContact = Object.create(Contact);
-      testContact.initialize("Mary", "Jane");
-      testContact.firstName.should.equal("Mary");
-      testContact.lastName.should.equal("Jane");
+      var testCategory = Object.create(Category);
+      testCategory.initialize("food");
+      testCategory.category.should.equal("food");
     });
 
-    it("sets up an empty array for the addresses property", function() {
-       var testContact = Object.create(Contact);
-       testContact.initialize("Mary", "Jane");
-       testContact.addresses.should.eql([]);
-    });
-  });
-});
-////////////////////////////////////////
-describe('Address', function() {
-  describe("create", function() {
-    it("creates an instance of the address object", function() {
-      var testAddress = Address.create();
-      Address.isPrototypeOf(testAddress).should.equal(true);
-    });
-
-    it("initializes the address instance", function() {
-      var testAddress = Address.create();
-      testAddress.initialize("5th", "Portland", "Oregon");
-      testAddress.state.should.equal("Oregon");
-    });
-  });
-
-  describe("initialize", function() {
-    it("initializes the address", function() {
-      var testAddress = Object.create(Address);
-      testAddress.initialize("5th", "Portland", "Oregon");
-      testAddress.street.should.equal("5th");
-      testAddress.city.should.equal("Portland"); 
-      testAddress.state.should.equal("Oregon");   
-    });
-  });
-
-  describe('fullAddress', function() {
-    it('should return the address of the given contact', function() {
-      var testAddress = Object.create(Address);
-      testAddress.street = "4444 SW 5th";
-      testAddress.city = "Portland";
-      testAddress.state = "Oregon";
-      testAddress.fullAddress().should.equal("4444 SW 5th, Portland, Oregon");
-    });
-
-    describe('validateAddress', function() {
-      it('should validate the address input', function() {
-        var testAddress = Object.create(Address);
-        testAddress.street = "";
-        testAddress.city = "Portland";
-        testAddress.state = "Oregon";
-        testAddress.validateAddress().should.equal(false);      
-      });
-    });
-  });
-});
-
-/////////////////////////////////////////
-describe('Phone', function() {
-  describe('create', function() {
-    it('should create an instance of Phone', function() {
-      var testPhone = Phone.create();
-      Phone.isPrototypeOf(testPhone).should.equal(true);
-    });
-
-    it('should initialize an instance of Phone', function() {
-      var testPhone = Object.create(Phone);
-      testPhone.initialize("1234567890");
-      testPhone.number.should.equal("1234567890");
-    });
-  });
-  describe("initialize", function() {
-    it("initializes the phone", function() {
-      var testPhone = Object.create(Phone);
-      testPhone.initialize("1234567890");
-      testPhone.number.should.equal("1234567890");   
-    });
-  });
-  describe('showNumber', function() {
-    it('should return the phone number of the given contact', function() {
-      var testPhone = Object.create(Phone);
-      testPhone.number = "562";
-      testPhone.showNumber().should.equal("562");
-    });
-  });
-  describe('validateNumber', function() {
-    it('should check to see if the phone number field is empty', function() {
-      var testValidateNumber = Object.create(Phone);
-      testValidateNumber.number = "";
-      testValidateNumber.validateNumber().should.equal(false);
-    });
-
-    it('should check to see that numbers are entered for the phone number', function() {
-      var testValidateNumber = Object.create(Phone);
-      testValidateNumber.number = "asdfsadf";
-      testValidateNumber.validateNumber().should.equal(false);
+    it("sets up an empty array for the purchase property", function() {
+       var testCategory = Object.create(Category);
+       testCategory.initialize("food");
+       testCategory.purchase.should.eql([]);
     });
   });
 });
 
 
+describe('Purchase', function() {
+   describe("create", function() {
+    it("creates an instance of the purchase object", function() {
+      var testPurchase = Purchase.create();
+      Purchase.isPrototypeOf(testPurchase).should.equal(true);
+    });
+     it("initializes the purchase", function() {
+      var testPurchase = Purchase.create("Burger", "10");
+      testPurchase.initialize("Burger","10");
+      testPurchase.description.should.equal("Burger");
+      testPurchase.amount.should.equal("10");
+    });
+  });
+   describe("initialize", function() {
+    it("sets the description and amount", function() {
+      var testPurchase = Object.create(Purchase);
+      testPurchase.initialize("Burger", "10");
+      testPurchase.description.should.equal("Burger");
+      testPurchase.amount.should.equal("10"); 
+    });
+  });
+});
 
-// describe('function name', function() {
-//   it('what you want the function to do', function() {
-//     caps("input").should.equal("output");
-//   });
-// });
